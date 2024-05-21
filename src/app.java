6f2ed1b98af256b040ca5144328b53d58e08423f
JT-808 <jerome.tengg@gmx.de>
2024-05-21 17:36:47 +0200

    init Pomopal

---
+++ src/app.java
@@ -0,0 +1,32 @@
+package src;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class app extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+            Scene scene = new Scene(root);
+            String css = this.getClass().getResource("gui.css").toExternalForm();
+            scene.getStylesheets().addAll(css);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
