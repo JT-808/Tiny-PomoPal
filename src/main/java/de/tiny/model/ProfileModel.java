3e5bc1738398da39477220866fe759006dc99506
JT-808 <jerome.tengg@gmx.de>
2024-07-29 17:37:44 +0200

    screeshot

--- src/main/java/de/tiny/model/ProfileModel.java
+++ src/main/java/de/tiny/model/ProfileModel.java
@@ -3,7 +3,6 @@ package src.main.java.de.tiny.model;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 public class ProfileModel implements Serializable {
     private final HashMap<String, List<Integer>> profiles = new HashMap<>();
